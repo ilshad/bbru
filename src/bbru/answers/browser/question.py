1e8da1d622e7c5f6f0c8c108a7b47030acaf98de
Ilshad Khabibullin <astoon.net@gmail.com>
2010-05-03 11:16:50 +0600

    user-icon fixed

--- src/bbru/answers/browser/question.py
+++ src/bbru/answers/browser/question.py
@@ -2,4 +2,19 @@
 # This code was developed for http://bluebream.ru by its community and
 # placed under Public Domain.
 
-class Pagelet: pass
+from zope.component import getUtility
+from zope.authentication.interfaces import IAuthentication
+from zope.dublincore.interfaces import IZopeDublinCore
+
+class Pagelet:
+
+    def update(self):
+        formatter = self.request.locale.dates.getFormatter('date')
+
+        dc = IZopeDublinCore(self.context)
+        auth = getUtility(IAuthentication)
+        
+        principal = auth.getPrincipal(dc.creators[0])
+        self.user = principal.title
+
+        self.created = formatter.format(dc.created)
