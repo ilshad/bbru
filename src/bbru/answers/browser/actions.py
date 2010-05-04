be1cbb99631a98bf0496972ee1eeb38859793727
Ilshad Khabibullin <astoon.net@gmail.com>
2010-05-04 13:44:47 +0600

    added explicit login actions

---
+++ src/bbru/answers/browser/actions.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+# This code was developed for http://bluebream.ru by its community and
+# placed under Public Domain.
+
+import urllib
+from zope.component.hooks import getSite
+from zope.security import checkPermission
+from zope.traversing.browser.absoluteurl import absoluteURL
+from z3c.menu.ready2go.item import ContextMenuItem
+
+class FilterPermissionAction(ContextMenuItem):
+    """ Класс для элементов меню, доступных только тогда,
+    когда не хватает определенного разрешения. Его можно указать
+    позиционным аргументом filter_permission в zcml-директиве.
+    """
+
+    filter_permission = None # указать в zcml-директиве
+
+    @property
+    def available(self):
+        return not checkPermission(self.filter_permission, self.context)
+
+class LoginAction(FilterPermissionAction):
+    """ Обычно это не требуется, т.к. фреймворк сам перенаправляет
+    на страницу авторизации, если не хватает прав. Однако иногда
+    полезно сделать элемент меню с прямо заданным подобным поведением,
+    например для Ajax-форм.
+    """
+
+    next_name = None # указать в zcml-директиве
+
+    @property
+    def url(self):
+        context_url = absoluteURL(self.context, self.request)
+
+        return u'%s/loginForm.html?camefrom=%s' % (
+            context_url, context_url + u'/' + self.next_name)
